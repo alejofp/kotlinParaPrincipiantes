e0c1b7ec287321976993870c005f1e305a7f7007
alejofp <92225725+alejofp@users.noreply.github.com>
2021-10-09 20:24:33 +0200

    Initial commit

---
+++ app/src/main/java/com/alejofp/kotlinparaprincipiantes/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.alejofp.kotlinparaprincipiantes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        variablesYconstantes()
+        tiposDeDatos()
+    }
+
+    private fun variablesYconstantes( )  {
+
+        //Variables
+
+        var myFirstVariable = "Hola Mundo Cruel"
+        var myFirstNumber = 1
+        var mySecondVariable = "que passsssa"
+
+
+
+        println(myFirstVariable)
+
+        myFirstVariable = "por que no imprimes?"
+
+        println(myFirstVariable)
+        println(myFirstNumber)
+
+
+        //Constantes
+
+        val myFirstConstant = "soy una constante"
+
+        println (myFirstConstant)
+
+  //      myFirstConstant = myFirstVariable
+
+
+    }
+
+
+    private fun tiposDeDatos() {
+
+
+        val myString: String = "soy una cadena de caracteres"
+
+        val myString2: String = myString + "  " + myString
+
+        println (myString2)
+
+
+
+
+
+    }
+
+}
